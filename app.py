9df0af9c15545d67df18f5019a0e76dde1a5edfc
yogeshnile <yogeshnile786@gmail.com>
2020-08-10 14:18:41 +0530

    Flask app ready

---
+++ app.py
@@ -0,0 +1,54 @@
+#Import Dependentias
+from flask import Flask, render_template, request, redirect
+import pickle
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+nltk.download('stopwords')
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+
+#Import Pickle file
+file_name = "Restaurant-reviews-model.pkl"
+classifier = pickle.load(open(file_name, 'rb'))
+
+file_name = "corpus.pkl"
+corpus = pickle.load(open(file_name, 'rb'))
+
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+
+def predict_review(sample_message):
+    sample_message = re.sub(pattern='[^a-zA-Z]',repl=' ', string = sample_message)
+    sample_message = sample_message.lower()
+    sample_message_words = sample_message.split()
+    sample_message_words = [word for word in sample_message_words if not word in set(stopwords.words('english'))]
+    ps = PorterStemmer()
+    final_message = [ps.stem(word) for word in sample_message_words]
+    final_message = ' '.join(final_message)
+    temp = cv.transform([final_message]).toarray()
+    return classifier.predict(temp)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/result', methods=['POST','GET'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        if not message == "":
+            if predict_review(message):
+                return render_template('index.html', result = 1, message = message) #positive
+            else:
+                return render_template('index.html', result = 0, message = message) #negative
+        else:
+            return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
